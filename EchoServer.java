6fc5369a7ff53fdb2464c4ab13c9313e43abde97
Ben Chin <bechin@cpp.edu>
2016-02-05 11:31:02 -0800

    Implemented multiple simultaneous clients.

--- EchoServer.java
+++ EchoServer.java
@@ -9,24 +9,31 @@ import java.net.Socket;
 
 public final class EchoServer {
 
-    public static void main(String[] args) throws Exception {
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
-            while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os);
-		    InputStream is = socket.getInputStream();
-         	    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-         	    BufferedReader br = new BufferedReader(isr);
-		    String message;
-		    while((message = br.readLine())!=null){
-			out.println(message);
-		    }
-		    System.out.printf("Client disconnected: %s%n", address);
-                }
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+        	try (ServerSocket serverSocket = new ServerSocket(22222)) {
+	            	while (true) {
+	       	       		Socket socket = serverSocket.accept();
+				Runnable client = () -> {
+					try{
+						String address = socket.getInetAddress().getHostAddress();
+						System.out.printf("Client connected: %s%n", address);
+						OutputStream os = socket.getOutputStream();
+				               	PrintStream out = new PrintStream(os);
+						InputStream is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+				       	    	BufferedReader br = new BufferedReader(isr);
+					    	String message;
+						while((message = br.readLine())!=null){
+							out.println(message);
+					    	}
+					    	System.out.printf("Client disconnected: %s%n", address);
+						socket.close();
+					}
+					catch (Exception e){}
+			       };
+			       Thread clientThread = new Thread(client);
+			       clientThread.start();
+            		}
+		}
+	}
 }
